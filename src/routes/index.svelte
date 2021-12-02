fbcb033b50d48de57f1ed00228eb7a06929c5bbb
acou12 <51276518+acou12@users.noreply.github.com>
2021-12-01 19:27:28 -0700

    Initial commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<p>
+  This text should be black, the 
+  default p color. Now go to 
+  <a href="/pink">/pink</a>.
+</p>
---
+++ src/routes/pink.svelte
@@ -0,0 +1,21 @@
+<script>
+  function dynamicallyGeneratedContent() {
+    return "<p>Dynamically generated content</p>";
+  }
+</script>
+
+<p>{@html dynamicallyGeneratedContent()}</p>
+<p>Now press the back button.</p>
+
+<style>
+  :global(p) {
+    color: pink;
+  }
+  /*
+  Doesn't work; the style is not applied to 
+  the dynamically generated content.
+  
+  p {
+    color: pink;    
+  } */
+</style>
